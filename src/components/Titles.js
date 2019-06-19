533fcd6a7e685c8fe6d6e2c6184ed0b805989871
abhishek-dangol <abhi.dangol@gmail.com>
2019-06-19 13:35:02 -0500

    initial commit

---
+++ src/components/Titles.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class Titles extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1 className="title-container__title">Weather Finder</h1>
+        <h3 className="title-container__subtitle">
+          Find Out Temperature, Conditions And More..
+        </h3>
+      </div>
+    );
+  }
+}
+
+export default Titles;
